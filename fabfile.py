d4e2d62a38a41f481d9d2723070db1374514435d
root <root@PC-macksoft.(none)>
2013-02-18 10:41:17 +0000

    fabfile.py~ 	servers/tests.pyc

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from __future__ import with_statement
 from fabric.api import * 
 from fabric.operations import run,put
 
-env.hosts = ['localhost']
+env.hosts = ['my_server']
 def test():
     local("./manage.py test servers")
 
