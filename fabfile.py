a896ce6231fb24e8dddd9aee1380bc8e9e9654e5
manga <macksoft2@gmail.com>
2013-02-18 11:11:49 +0000

    fabfile.py~ 	servers/tests.pyc

--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,7 @@ def test():
 def commit():
     local("git add -p && git commit ")
     #local("git remote add origin git@github.com:manga/servers.git")
-    local("git origin master ")    
+    local("git push origin master ")    
 
 
 
