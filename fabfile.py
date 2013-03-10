33e8b4da71616ceae0411dcddf334eacd6a6af23
daouda <macksoft2@gmail.com>
2013-03-10 22:53:11 +0000

    modified:   .idea/workspace.xml 	modified:   fabfile.py 	modified:   fabfile.pyc

--- fabfile.py
+++ fabfile.py
@@ -13,7 +13,9 @@ def deploy():
     local(" git commit ")
     local("git push ")
     code_dir = '/home/manga/deplyapp'
-    local("git clone git@github.com:macksoft2/servers.git %s" % code_dir)
+    with settings(warn_only=True):
+        if local("test -d %s" % code_dir):
+            local("git clone git@github.com:macksoft2/servers.git %s" % code_dir)
     with cd(code_dir):
-         local("git pull ")
-         local("touch app.wsgi")
+        local("git pull ")
+        local("touch app.wsgi")
