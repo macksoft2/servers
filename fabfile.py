eb11e630d27c461ee801e25ecd88269047b6b8eb
daouda <macksoft2@gmail.com>
2013-03-11 10:05:37 +0000

    modified:   fabfile.pyc

--- fabfile.py
+++ fabfile.py
@@ -4,21 +4,7 @@ from fabric.api import *
 from fabric.operations import run,env,local,put
 env.hosts = ['192.168.1.45']
 
-def test():
+def prepare_deploy():
     local("./manage.py test servers")
-def commit():
-    local("git add && git commit -a")
-    #local("git remote add origin git@github.com:macksoft2/servers.git")
-    local("git push -f")
-def deploy():
-    #sudo("apt-get install git ")
-    #sudo("mkdir testdeploy")
-    test()
-    commit()
-    code_dir = '/home/deplyapp'
-    with settings(warn_only=True):
-        if local("test -d %s" % code_dir):
-            local("git clone git@github.com:macksoft2/servers.git %s" % code_dir)
-    with cd(code_dir):
-        local("git pull origin master")
-        local("touch app.wsgi")
+    local("git add -p && git commit")
+    local("git push")
