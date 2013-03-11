3b6af209a323b240171ddf6be5c90e118a251409
daouda <macksoft2@gmail.com>
2013-03-11 09:24:52 +0000

    modified:   fabfile.py 	modified:   fabfile.pyc

--- fabfile.py
+++ fabfile.py
@@ -2,14 +2,14 @@
 from __future__ import with_statement
 from fabric.api import *
 from fabric.operations import run,env,local,put
-env.hosts = ['192.168.1.45']
+env.hosts = ['192.168.1.7']
 env.user = ['root']
 
 def test():
     local("./manage.py test servers")
 def commit():
     local("git add && git commit -a")
-    #local("git remote add origin git@github.com:macksoft2/servers.git")
+    local("git remote add origin git@github.com:macksoft2/servers.git")
     local("git push -f")
 def deploy():
     test()
