2d46ceaaa09fce1aeff975e8c9ccd4d0cee0254f
daouda <macksoft2@gmail.com>
2013-03-11 10:16:38 +0000

    modified:   .idea/workspace.xml 	modified:   fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import with_statement
 from fabric.api import *
-from fabric.operations import run,env,local,put
+from fabric.operations import run,env,local
+from fabric.contrib.console import confirm
 env.hosts = ['192.168.1.45']
+def test():
+    with settings(warn_only=True):
+        result = local('./manage.py test servers', capture=True)
+    if result.failed and not confirm("Tests failed. Continue anyway?"):
+        abort("Aborting at user request.")
+
+def commit():
+    local("git add -p && git commit")
+
+def push():
+    local("git push")
 
 def prepare_deploy():
-    local("./manage.py test servers")
-    local("git add -p && git commit")
-    local("git push")
+    test()
+    commit()
+    push()
+
+def deploy():
+    code_dir = '/home/manga/deplyapp'
+    with settings(warn_only=True):
+        if local("test -d %s" % code_dir).failed:
+            local("git clone git@github.com:macksoft2/servers.git %s" % code_dir)
+    with cd(code_dir):
+        local("git pull")
+        local("touch app.wsgi")
