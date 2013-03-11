c60f5aa1affcdd12539833f6aeab1aa45bb2e9f6
daouda <macksoft2@gmail.com>
2013-03-11 10:59:33 +0000

    modified:   .idea/workspace.xml 	modified:   fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -3,12 +3,33 @@ from __future__ import with_statement
 from fabric.api import *
 from fabric.operations import run,env,local
 from fabric.contrib.console import confirm
+import fabtools.require
+from fabtools.python_distribute import is_distribute_installed, install_distribute
+from fabtools.require.python import  *
 env.hosts = ['192.168.1.45']
 def test():
     with settings(warn_only=True):
         result = local('./manage.py test servers', capture=True)
     if result.failed and not confirm("Tests failed. Continue anyway?"):
         abort("Aborting at user request.")
+
+def install_packages():
+
+    fabtools.require.deb.packages([
+                                      'python-dev',
+                                      'curl'
+                                  ], update=False
+    )
+    # installation de python
+    if not  is_distribute_installed():
+        install_distribute()
+    # installation de pip
+    if not is_pip_installed():
+        install_pip()
+
+def install_mysql():
+    #installtion de mysql
+    fabtools.require.mysql.server(password='passer12')
 
 def commit():
     local("git add -p && git commit")
@@ -22,10 +43,12 @@ def prepare_deploy():
     push()
 
 def deploy():
-    code_dir = '/home/manga/deplyapp'
+    code_dir = '/deploymanga'
+    sudo("mkdir -p deploymanga")
+    cd("deploymanga")
     with settings(warn_only=True):
-        if local("test -d %s" % code_dir).failed:
-            local("git clone git@github.com:macksoft2/servers.git %s" % code_dir)
+        if run("test -d %s" % code_dir).failed:
+            run("git clone git@github.com:macksoft2/servers.git %s" % code_dir)
     with cd(code_dir):
-        local("git pull")
-        local("touch app.wsgi")
+        run("git pull origin master")
+        run("touch app.wsgi")
