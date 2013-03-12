0321c3a8413df0f417ee7e16822c4e3c2cb953b6
daouda <macksoft2@gmail.com>
2013-03-12 11:51:10 +0000

    modified:   fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -38,7 +38,7 @@ def install_mysql(mdp="passer"):
 def commit():
     local("git add . && git commit")
 def push():
-    sudo("git push ")
+    local("git push ")
 #--------------------------------------------- End dev fonctions-----------------------------------#
 
 @hosts('localhost:8000')
@@ -48,7 +48,7 @@ def prepare_deploy():
 
 def deploy():
     local('ssh-copy-id root@192.168.1.45')
-    run("cd /root/manga  & git clone git@github.com:macksoft2/servers.git %s" % code_dir)
+    sudo("cd /root/manga  & git clone git@github.com:macksoft2/servers.git %s" % code_dir)
     with cd(code_dir):
         run("git pull origin master")
         run("touch myapp.wsgi")
